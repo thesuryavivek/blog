3e774a73a46b3d78f38d28bfb0dc19ff28409813
suryavivek <suryavivek33377@gmail.com>
2023-09-20 17:40:07 +0530

    Intro placeholder and Projects section

---
+++ src/components/Intro.md
@@ -0,0 +1,3 @@
+Lorem ipsum - dolor sit amet consectetur adipisicing elit. Eaque, ea. Rerum
+consequatur necessitatibus earum, ipsam quaerat sit voluptas nemo optio
+pariatur saepe cupiditate nisi, commodi neque nulla ex debitis amet.
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,38 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+import CountriesAppImage from "./assets/countries_app.png";
+import DevNetworkImage from "./assets/dev_network.png";
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Surya Vivek";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+
+// Projects
+export type ProjectType = {
+  name: string;
+  desc: string;
+  tech: string[];
+  image: ImageMetadata;
+};
+
+export const DEV_NETWORK: ProjectType = {
+  name: "Dev Network",
+  desc: "A social media app excusively for developers to post stuff and chat with one another.",
+  image: DevNetworkImage,
+  tech: [
+    "tRPC",
+    "Next.js",
+    "Tailwind CSS",
+    "Websockets",
+    "OAuth",
+    "MySQL",
+    "Primsa",
+    "TypeScript",
+  ],
+};
+
+export const COUNTRIES_APP: ProjectType = {
+  name: "Countries App",
+  desc: "A frontend application that consumes a third party REST API to display and filter the data.",
+  image: CountriesAppImage,
+  tech: ["Svelte", "Tailwind CSS", "TypeScript"],
+};
